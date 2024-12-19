b27e7479066b534b87019fc35a1679026377cbcd
prak132 <rathoreprakhar1@gmail.com>
2024-12-18 20:17:27 -0800

    Fix for formatting check CI

--- src/frc846/cpp/frc846/wpilib/NTAction.cc
+++ src/frc846/cpp/frc846/wpilib/NTAction.cc
@@ -7,7 +7,8 @@ namespace frc846::wpilib {
 NTAction::NTAction(std::function<void()> callback) : callback_(callback) {}
 void NTAction::InitSendable(wpi::SendableBuilder& builder) {
   builder.SetSmartDashboardType("Command");
-  builder.AddStringProperty(".name", [] { return "Run"; }, nullptr);
+  builder.AddStringProperty(
+      ".name", [] { return "Run"; }, nullptr);
   builder.AddBooleanProperty(
       "running", [] { return false; },
       [this](bool value) {
